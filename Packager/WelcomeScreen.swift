14a83dca86596821d479e1c1cad7cb0ce12c602e
Gijsbert te Paske <gstpsk@Gijsberts-iMac.local>
2019-02-11 19:01:39 +0100

    Fixed constraints

---
+++ Packager/WelcomeScreen.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomeScreen.swift
+//  Packager
+//
+//  Created by Gijsbert te Paske on 11/02/2019.
+//  Copyright © 2019 Conor Byrne. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
